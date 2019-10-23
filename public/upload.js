cb3e03d940324801a9faea4e5a704b17a2dfe5e1
Matt D <dwinell.matt@gmail.com>
2019-10-23 15:51:54 -0400

    breaking current iteration of bar graph to work on bar rendering

--- public/upload.js
+++ public/upload.js
@@ -244,18 +244,77 @@ function binSortForBar(obj, colNum) {
 function generateBar(obj) {
   console.log(obj);
 
-  var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-  //appending an svg element to the bar wrapper div
-  var svg = d3.select("#bar-wrapper")
-    .append("svg")
+// append the svg object to the body of the page
+var svg = d3.select("#bar-wrapper")
+  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+  .append("g")
     .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+var data = obj;
+
+
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.colNames )
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data.values)])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.Country); })
+      .attr("y", function(d) { return y(d.Value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.Value); })
+      .attr("fill", "#69b3a2")
+
+
+
+
+
+
+
+
+
+
+  // var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+  //   width = 460 - margin.left - margin.right,
+  //   height = 400 - margin.top - margin.bottom;
+
+  //appending an svg element to the bar wrapper div
+  // var svg = d3.select("#bar-wrapper")
+  //   .append("svg")
+  //   .attr("width", width + margin.left + margin.right)
+  //   .attr("height", height + margin.top + margin.bottom)
+  //   .append("g")
+  //   .attr("transform",
+  //     "translate(" + margin.left + "," + margin.top + ")");
 
   // Parse the Data
   // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
@@ -266,16 +325,16 @@ function generateBar(obj) {
   // });
 
   // X axis
-  var x = d3.scaleBand()
-    .range([0, width])
-    .domain(obj.colNames)
-    .padding(0.2);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  // var x = d3.scaleBand()
+  //   .range([0, width])
+  //   .domain(obj.colNames)
+  //   .padding(0.2);
+  // svg.append("g")
+  //   .attr("transform", "translate(0," + height + ")")
+  //   .call(d3.axisBottom(x))
+  //   .selectAll("text")
+  //   .attr("transform", "translate(-10,0)rotate(-45)")
+  //   .style("text-anchor", "end");
 
   // Add Y axis
 //   var y = d3.scaleLinear()
